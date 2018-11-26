20088e74d5e4a5e7776541933c54ca7812c277b3
panpeidong <1761464341@qq.com>
2018-11-26 15:07:55 +0800

    新增示例代码

---
+++ Simple01/Simple01/Controllers/ContentController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Simple01.Models;
+
+namespace Simple01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+        public ContentController(IOptions<Content> option)
+        {
+            contents = option.Value;
+        }
+
+        public IActionResult Index()
+        {
+            //var contents = new List<Content>();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    contents.Add(new Content { Id = i, Title = $"{i}的标题", Contents = $"{i}的内容", State = 1, AddTime = DateTime.Now.AddDays(-1) });
+            //}
+            //return View(new ContentViewModel { Contents = contents });
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
---
+++ Simple01/Simple01/Models/Content.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Simple01.Models
+{
+    public class Content
+    {
+        /// <summary>
+        /// 主键
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 内容
+        /// </summary>
+        public string Contents { get; set; }
+
+
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int State { get; set; }
+
+        /// <summary>
+        /// 添加日期
+        /// </summary>
+        public DateTime AddTime { get; set; }
+
+        /// <summary>
+        /// 修改日期
+        /// </summary>
+        public DateTime ModifyTime { get; set; }
+
+    }
+}
